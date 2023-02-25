a0f23c7587da91086023691b607b1ae16f897503
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-25 16:03:43 +0530

    import project components into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Projects";
 
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <Router>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
       </Router>
     </div>
   );
