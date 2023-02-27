423ae112ab6fef0780e85e3fdf240341f8fa4a3a
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-27 09:25:03 +0530

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <LinkedInIcon />
+      </div>
+      <p> &copy; shaunjiji.com </p>
+    </div>
+  );
+}
+
+export default Footer;
