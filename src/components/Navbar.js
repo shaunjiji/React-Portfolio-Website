7b501189736075a77defe149ccba925257bb7559
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-27 08:46:53 +0530

    import reordericon and implement state for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
   return (
     <div className="navbar">
       <div className="toggleButton">
-        <button></button>
+        <button>
+          <ReorderIcon />
+        </button>
       </div>
-      <div className="links"></div>
-      <Link to="/"> Home</Link>
-      <Link to="/projects"> Projects</Link>
-      <Link to="/experience"> Experience</Link>
+      <div className="links">
+        <Link to="/"> Home</Link>
+        <Link to="/projects"> Projects</Link>
+        <Link to="/experience"> Experience</Link>
+      </div>
     </div>
   );
 }
