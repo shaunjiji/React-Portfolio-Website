521c13034f1214fb381960c24ee66dc89e92f2ec
Shaun Jiji <shaunjiji123@gmail.com>
2023-02-28 09:10:49 +0530

    add verticaltimeline element

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,23 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+  VerticalTimeline,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2016 - 2021"
+          iconStyle={{ background: "#3e497", colo: "fff" }}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
